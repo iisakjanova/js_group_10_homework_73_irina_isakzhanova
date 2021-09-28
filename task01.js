fe5a08a300c352418e1377d0630d9eeb76270c0b
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-09-28 10:11:37 +0600

    Task 01 is done

---
+++ task01.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+app.get('/:text', (req, res) => {
+    res.send(req.params.text);
+});
+
+app.listen(port, () => {
+    console.log('We are live in ' + port);
+});
